7edd998869e4ba1498495130bab2af21311b2563
Pramuda Liyanage <pramudatharika@gmail.com>
2021-12-03 22:40:40 +0530

    Dependencies Add

---
+++ Spring-Security/src/main/java/lk/ijse/Spring/Security/WebSecurityConfig.java
@@ -0,0 +1,5 @@
+package lk.ijse.Spring.Security;/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 12/3/21
+ **/public class WebSecurityConfig {
+}
---
+++ Spring-Security/src/main/java/lk/ijse/Spring/controller/CustomerFormController.java
@@ -0,0 +1,8 @@
+package lk.ijse.Spring.Security.controller;
+
+/**
+ * @author Pramuda Liyanage <pramudatharika@gmail.com>
+ * @since 12/3/21
+ **/
+public class CustomerFormController {
+}
